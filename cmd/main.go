4324560d7b827fadcd3959de55e8f7e232d3ced3
Maksim-Gol <maks-golb@yandex.ru>
2024-07-04 15:10:22 +0300

    Configured logger and config

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,41 @@ package main
 
 import (
 	"github.com/Maksim-Gol/neuralService/handlers"
+	"github.com/Maksim-Gol/neuralService/internal/config"
 	"github.com/gofiber/fiber/v2"
+	"log/slog"
+	"os"
 )
 
 
+const (
+	envLocal = "local"
+	envProd = "dev"
+	envDev = "prod"
+)
+
 func main(){
+	// TODO: config clenaenv
+	cfg := config.MustLoad()
+	// TODO: logger slog
+	log := setupLogger(cfg.Env)
+	log.Info("Start neuralService", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled")
 	app := fiber.New()
 
 	handlers.RegisterRoutes(app)
-	app.Listen(":3000")
+	app.Listen(cfg.Port)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	}
+	return log
+
 }
